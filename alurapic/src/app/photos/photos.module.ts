12925da2bcc499b62b46ad7d3cd3a6472bc73596
Nicolas <nicolas@MacBook-2013.local>
2018-07-22 13:49:31 -0300

    ang 1.4.1

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
     declarations: [
-        PhotoComponent
+        PhotoComponent,
+        PhotoListComponent
     ],
     imports: [
       HttpClientModule
-    ],
-    exports: [
-        PhotoComponent
     ]
 })
 export class PhotosModule { }
