9de48adfdfa6fa9e917c971d7164859204fd629a
Nicolas <nicolas@MacBook-2013.local>
2018-08-17 16:53:50 -0300

    ang 2.1.6

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { SigninComponent } from './signin/signin.component';
+import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [ SigninComponent ],
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        VmessageModule
     ],
     exports: [],
     providers: [],
