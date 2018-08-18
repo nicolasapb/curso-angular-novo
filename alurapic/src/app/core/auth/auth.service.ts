ea247a38addc24dc029829e8769d3a177b447f71
Nicolas <nicolas@MacBook-2013.local>
2018-08-18 20:22:41 -0300

    ang 2.2.4

--- alurapic/src/app/core/auth/auth.service.ts
+++ alurapic/src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 const API_URL = 'http://localhost:3000/';
 @Injectable({
@@ -8,7 +9,9 @@ const API_URL = 'http://localhost:3000/';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService) { }
 
   authenticate(userName: string, password: string) {
 
@@ -20,6 +23,7 @@ export class AuthService {
       )
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
         console.log(`usuer ${userName} authenticated with token ${authToken}`);
       }));
   }
