6108f9a89cd85d625b662b7f305f14dd2deed201
Nicolas <nicolas@MacBook-2013.local>
2018-08-19 14:14:22 -0300

    ang 2.4.3

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -1,13 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './signup.component.html'
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+      this.signupForm = this.formBuilder.group({
+        email: ['', [
+          Validators.required,
+          Validators.email
+        ]],
+        fullName: ['', [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]],
+        userName: ['', [
+          Validators.required,
+          Validators.pattern(/^[a-z0-9_\-]+$/),
+          Validators.minLength(2),
+          Validators.maxLength(10)
+        ]],
+        password: ['', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(14),
+        ]]
+      });
   }
 
 }
