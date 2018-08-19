977571808ccbafecdbff7dab21743a32a57cdea4
Nicolas <nicolas@MacBook-2013.local>
2018-08-19 12:13:59 -0300

    ang 2.3.7

--- alurapic/src/app/core/header/header.component.ts
+++ alurapic/src/app/core/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user/user.service';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,8 +12,19 @@ export class HeaderComponent implements OnInit {
 
   user$: Observable<User>;
 
-  constructor(private userService: UserService) {
+  constructor(
+      private userService: UserService,
+      private router: Router) {
     this.user$ = userService.getUser();
+  }
+
+  logout() {
+    this.userService.logout();
+    this.goHome();
+  }
+
+  goHome() {
+    this.router.navigate(['']);
   }
 
   ngOnInit() {}
