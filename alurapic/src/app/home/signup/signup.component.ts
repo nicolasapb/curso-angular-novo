795bcaf74babf1cce5fda61d16ef95463332523b
Nicolas <nicolas@MacBook-2013.local>
2018-08-23 21:21:22 -0300

    ang 2.4.8

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { lowerCaseValidator } from '../../shared/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { NewUser } from './new-user';
+import { SignUpService } from './signup.service';
+import { Router } from '@angular/router';
+import { PlatformDetectorService } from '../../core/platform-detector/platform-detector.service';
 
 @Component({
   templateUrl: './signup.component.html'
@@ -9,10 +13,14 @@ import { UserNotTakenValidatorService } from './user-not-taken.validator.service
 export class SignupComponent implements OnInit {
 
   signupForm: FormGroup;
+  @ViewChild('#emailInput') emailInput: ElementRef<HTMLInputElement>;
 
   constructor(
     private formBuilder: FormBuilder,
-    private userNotTakenValidatorService: UserNotTakenValidatorService) { }
+    private userNotTakenValidatorService: UserNotTakenValidatorService,
+    private signupService: SignUpService,
+    private router: Router,
+    private platformDetectorService: PlatformDetectorService) { }
 
   ngOnInit() {
     this.signupForm = this.formBuilder.group({
@@ -42,7 +50,22 @@ export class SignupComponent implements OnInit {
   }
 
   signup() {
-    const newUser = this.signupForm.getRawValue();
+    const newUser = this.signupForm.getRawValue() as NewUser;
+    this.signupService
+        .signup(newUser)
+        .subscribe(
+          () => {
+            this.router.navigate(['']);
+            alert('new user created with success');
+          },
+          err => {
+            console.log('erro', err);
+            this.signupForm.reset();
+            // tslint:disable-next-line:no-unused-expression
+            this.platformDetectorService.isPlatformBrowser() &&
+                this.emailInput.nativeElement.focus();
+            alert('Something went wrong ):');
+          });
   }
 
 }
