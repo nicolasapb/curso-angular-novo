bd34e84b31d70675062603eece6b3cc8cce7555e
Nicolas <nicolas@acnde906.la.interbrew.net>
2018-08-24 14:12:46 -0300

    ang 3.1.5

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PlatformDetectorService } from '../../core/platform-detector/platform-detector.service';
 
 @Component({
   templateUrl: './photo-form.component.html'
 })
 export class PhotoFormComponent implements OnInit {
 
-  constructor() { }
+  photoForm: FormGroup;
+  @ViewChild('descriptionInput') descriptionInput: ElementRef<HTMLInputElement>;
+
+  constructor(private formBuilder: FormBuilder,
+    private platformDetectorService: PlatformDetectorService) { }
 
   ngOnInit() {
+    this.photoForm = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true]
+    });
+
+    // tslint:disable-next-line:no-unused-expression
+    this.platformDetectorService.isPlatformBrowser() &&
+        this.descriptionInput.nativeElement.focus();
   }
 
 }
