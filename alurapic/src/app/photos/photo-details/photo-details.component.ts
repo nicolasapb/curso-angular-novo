2af52257c07fd26c99e949f81e492584cb59468b
Nicolas <nicolas@MacBook-2013.local>
2018-08-24 20:43:35 -0300

    ang 3.3.3

--- alurapic/src/app/photos/photo-details/photo-details.component.ts
+++ alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './photo-details.component.html'
 })
 export class PhotoDetailsComponent implements OnInit {
 
-  constructor() { }
+  photoId: number;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params.photoId;
+    console.log(id);
   }
 
 }
