a19bf92a255deb8fdd2fd75ab4e3d9249f9c3763
Nicolas <nicolas@acnde906.la.interbrew.net>
2018-08-24 13:53:14 -0300

    ang 3.1.4

--- alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { PhotoFormComponent } from './photo-form.component';
+import { VmessageModule } from '../../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [
         PhotoFormComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule,
+        VmessageModule,
+        FormsModule
     ]
 })
 export class PhotoFormModule {
