0f27ad6bd8f7bbb64f2f93f6461d45d269ff6c91
Nicolas <nicolas@acnde906.la.interbrew.net>
2018-08-27 08:57:57 -0300

    ang 3.6.7

--- alurapic/src/app/photos/photo-details/photo-details.component.ts
+++ alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
 import { Observable } from 'rxjs';
-import { PhotoComment } from '../photo/photo-comment';
+import { AlertService } from '../../shared/components/alert/alert.service';
 
 @Component({
   templateUrl: './photo-details.component.html'
@@ -16,7 +16,8 @@ export class PhotoDetailsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private photoService: PhotoService,
-    private router: Router) { }
+    private router: Router,
+    private alertService: AlertService) { }
 
   ngOnInit() {
     this.photoId = this.route.snapshot.params.photoId;
@@ -26,6 +27,15 @@ export class PhotoDetailsComponent implements OnInit {
   remove() {
     this.photoService
         .removePhoto(this.photoId)
-        .subscribe(() => this.router.navigate(['']));
+        .subscribe(
+          () => {
+            this.alertService.success('Photo Removed!');
+            this.router.navigate(['']);
+          },
+          err => {
+            console.log(err);
+            this.alertService.warning('Photo could not be removed!');
+          }
+        );
   }
 }
