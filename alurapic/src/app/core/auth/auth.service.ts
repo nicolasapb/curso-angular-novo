7357f561295a53cdef4200ba2f30c490d6a09520
Nicolas <nicolas@acnde906.la.interbrew.net>
2018-08-27 15:32:34 -0300

    ang 3.8.2

--- alurapic/src/app/core/auth/auth.service.ts
+++ alurapic/src/app/core/auth/auth.service.ts
@@ -3,7 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import { UserService } from '../user/user.service';
 
-const API_URL = 'http://localhost:3000/';
+import { environment } from '../../../environments/environment';
+
+const API = environment.ApiUrl;
 @Injectable({
   providedIn: 'root'
 })
@@ -17,7 +19,7 @@ export class AuthService {
 
     return this.http
       .post(
-        API_URL + 'user/login',
+        API + 'user/login',
         {userName, password},
         {observe: 'response'}
       )
