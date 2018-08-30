3d2d1f01d7fab961c2658f11241cb3c0c490b3e1
Nicolas <nicolas@MacBook-2013.local>
2018-08-29 21:29:19 -0300

    ang 4.4.6

--- alurapic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
+++ alurapic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -6,6 +6,8 @@ import { UserService } from '../../../core/user/user.service';
 })
 export class ShowIfLoggedDirective implements OnInit {
 
+    currentDisplay: string;
+
     constructor(
         private element: ElementRef<any>,
         private renderer: Renderer,
@@ -13,8 +15,15 @@ export class ShowIfLoggedDirective implements OnInit {
     ) {}
 
     ngOnInit(): void {
-        // tslint:disable-next-line:no-unused-expression
-        !this.userService.isLogged() &&
-            this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+
+        this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+        this.userService.getUser().subscribe(user => {
+            if (user) {
+                this.renderer.setElementStyle(this.element.nativeElement, 'display', this.currentDisplay);
+            } else {
+                this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+                this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+            }
+        });
     }
  }
