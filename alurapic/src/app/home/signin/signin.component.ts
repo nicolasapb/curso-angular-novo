17ccfdaef637574871dc2f5844c56b99b93fd7c4
Nicolas <nicolas@MacBook-2013.local>
2018-08-31 09:52:53 -0300

    ang 4.7.6 - final

--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../core/auth/auth.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { PlatformDetectorService } from '../../core/platform-detector/platform-detector.service';
 import { AlertService } from '../../shared/components/alert/alert.service';
 
@@ -10,6 +10,7 @@ import { AlertService } from '../../shared/components/alert/alert.service';
 })
 export class SigninComponent implements OnInit {
 
+    fromUrl: string;
     loginForm: FormGroup;
     loginInvalido = false;
     @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
@@ -19,9 +20,11 @@ export class SigninComponent implements OnInit {
         private authService: AuthService,
         private router: Router,
         private platformDetectorService: PlatformDetectorService,
-        private alertService: AlertService) { }
+        private alertService: AlertService,
+        private activatedRoute: ActivatedRoute) { }
 
     ngOnInit(): void {
+        this.activatedRoute.queryParams.subscribe(params => this.fromUrl = params['fromUrl']);
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
@@ -41,7 +44,9 @@ export class SigninComponent implements OnInit {
                 () => {
                     this.loginInvalido = false;
                     this.alertService.success('Welome ' + userName + '!', true);
-                    this.router.navigate(['user', userName]);
+                    this.fromUrl
+                        ? this.router.navigateByUrl(this.fromUrl)
+                        : this.router.navigate(['user', userName]);
                 },
                 err => {
                     console.log(err);
